3285b55df97da653c59aa90b99086d1943246399
DanielHalachev <dani22h09_pl@abv.bg>
2024-10-01 20:40:24 +0300

    Made Lighting an interface library

--- lighting/src/Utils.cpp
+++
@@ -1,7 +0,0 @@
-#include "Utils.h"
-
-// boost::multiprecision::mpfr_float_100 toRadians(boost::multiprecision::mpfr_float_100 degrees) {
-// 	boost::multiprecision::mpfr_float_100 oneHundredEighty = 180.0;
-// 	degrees = degrees / oneHundredEighty;
-// 	return degrees * boost::math::constants::pi<boost::multiprecision::mpfr_float_100>();
-// }
